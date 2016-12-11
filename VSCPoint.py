a20f989a56b5eaf46ae929757a3023c24fb93106
vscgit <valentinscheele@gmail.com>
2016-12-11 21:14:46 +0100

    First commit, initial version

--- VSCPoint.py
+++ VSCPoint.py
@@ -1,14 +1,73 @@
-#this code checks whether two lines intersect http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+# this code checks whether two lines intersect http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
 class Point:
-	def __init__(self,x,y):
-		self.x = x
-		self.y = y
-	def __str__(self):
-		return ("(" + str(self.x) + ":" + str(self.y)+")")
-	def __repr__(self):
-		return str(self)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def ccw(A,B,C):
-	return (C.y-A.y)*(B.x-A.x) > (B.y-A.y)*(C.x-A.x)
-def intersect(A,B,C,D):
-	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+    def __str__(self):
+        return ("(" + str(self.x) + "," + str(self.y) + ")")
+
+    def __repr__(self):
+        return str(self)
+
+def ccw(A, B, C):
+    return (C.y - A.y) * (B.x - A.x) >= (B.y - A.y) * (C.x - A.x)
+
+
+
+def intersection(s1, s2):
+        segment_endpoints = []
+        left = max(min(s1[0], s1[2]), min(s2[0], s2[2]))
+        right = min(max(s1[0], s1[2]), max(s2[0], s2[2]))
+        top = max(min(s1[1], s1[3]), min(s2[1], s2[3]))
+        bottom = min(max(s1[1], s1[3]), max(s2[1], s2[3]))
+
+        if top > bottom or left > right:
+            segment_endpoints = []
+            #print 'NO INTERSECTION'
+            #print segment_endpoints
+            return False
+
+        elif top == bottom and left == right:
+            segment_endpoints.append(left)
+            segment_endpoints.append(top)
+            #print 'POINT INTERSECTION'
+            #print segment_endpoints
+            return True
+        else:
+            segment_endpoints.append(left)
+            segment_endpoints.append(bottom)
+            segment_endpoints.append(right)
+            segment_endpoints.append(top)
+            #print 'SEGMENT INTERSECTION'
+            #print segment_endpoints
+            return True
+
+def intersect2(A, B, C, D):
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+
+
+def intersect3(A, B, C, D):
+    if A.x == B.x:
+        return not (C.x == D.x and A.x != C.x)
+    elif C.x == D.x:
+        return True
+    else:
+        m1 = (A.y - B.y) / (A.x - B.x)
+        m2 = (C.y - D.y) / (C.x - D.x)
+        return m1 != m2
+
+
+def intersect(A, B, C, D):
+    return intersection([A.x,A.y,B.x,B.y],[C.x,C.y,D.x,D.y])
+
+
+a = Point(0,0)
+b = Point(0,1)
+c = Point(1,1)
+d = Point(1,0)
+
+
+print intersect(a,b,c,d)
+print intersect(a,c,b,d)
+print intersect(a,d,b,c)
--- main.py
+++ main.py
@@ -4,11 +4,10 @@
 # Al Zimmerman's new Programming Contest
 # 1st try with drawing polygons and  some initial code
 #
-#
 #########################################
 
 
-POLYSEED=5  #max number of Polys generated from the old one in one iteration.
+POLYSEED=3 #max number of Polys generated from the old one in one iteration.
 solutions=[]
 
 import copy
@@ -19,15 +18,12 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 from VSCPoint import Point, intersect
 
-
-
-
 def PolyArea(x,y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
 # this method assumes that A.y differs from B.y
 def getslope(A,B):
-    return abs((float(A.y)-float(B.y)/(float(A.x)-float(B.x))))
+    return (float(A.y) - float(B.y)) / (float(A.x) - float(B.x))
 
 class Vscpoly:
     size=5
@@ -43,6 +39,9 @@ class Vscpoly:
         self.ytopop = range(1, size)
     def printPoly(self):
         return "polysize:"+str(len(self.selectedpoints))+"\tlinelength:"+str(self.length)+"\tpoints:"+str(self.selectedpoints)
+    def printPolyAli(self):
+        transpoints  = [ Point(p.x+1,p.y+1) for p in self.selectedpoints ]
+        return "polysize:"+str(len(self.selectedpoints))+"\tlinelength:"+str(self.length)+"\tpoints:"+str(self.selectedpoints)+"\tpoints:"+str(transpoints)
     def getPointlist(self):
         res=[]
         for p in self.selectedpoints:
@@ -60,9 +59,14 @@ class Vscpoly:
 def scorePoly(P):
     return P.slopesused.size
 
+#translates all points +1 because 1 to n
+def tra(list):
+    return [Point(p[0]+1, p[1] + 1) for p in list]
 
 def doesintersect(P1,P2,L): # Point, ListToCheck
     for i in range(0,len(L)-2):
+        test=intersect(P1,P2,L[i],L[i+1])
+       # print test,P1,P2,L[i],L[i+1]
         if intersect(P1,P2,L[i],L[i+1]) :
             return True
     return False
@@ -74,10 +78,15 @@ def mutateRandom(Poly1):  #gets Vscpoly and returns valid mutations from the ori
     ybucket=list(Poly1.ytopop)
     random.shuffle(xbucket)
     random.shuffle(ybucket)
-    while len(xbucket)>0 and len(result)<POLYSEED : # create up to Polyseed new Polygons
-        newx=xbucket.pop()
-        newy=ybucket.pop()
+    pointsbucket=[(x,y) for x in xbucket for y in ybucket]
+    while len(pointsbucket)>0 and len(result)<POLYSEED : # create up to Polyseed new Polygons
+        newpoint=pointsbucket.pop()
+        #newpoint=[(0, 0), (6, 1), (4, 2), (5, 3), (2, 6), (1, 4), (3, 5)][len(Poly1.selectedpoints)]
+        newx=newpoint[0]
+        newy=newpoint[1]
         newslope=getslope(Poly1.selectedpoints[-1], Point(newx, newy))
+        #if newx==4 and newy==4:
+        #    print "krit"
         if newslope not in Poly1.slopesused and not doesintersect(Poly1.selectedpoints[-1], Point(newx, newy), Poly1.selectedpoints): #new Slope not used and no intersection
             #lets create the new polygon!!
             newPoly=Poly1.getCopy()
@@ -90,7 +99,7 @@ def mutateRandom(Poly1):  #gets Vscpoly and returns valid mutations from the ori
             result.append(newPoly)
     return result
 
-#calculates the area of a Vscpoly with the help of Numpy magic
+#calculates the area of a Vscpoly with the help of Numpy package
 def findArea(P):
     x,y=[],[]
     for p in P.selectedpoints:
@@ -99,9 +108,8 @@ def findArea(P):
     return PolyArea(x,y)
 
 def drawPoly(Poly1):
-
     verts=Poly1.getPointlist()
-    verts.append((0.0,0.0))
+    verts.append((0,0))
     codes=[1]
     codes[0]=Path.MOVETO
     for v in range(0,len(verts)-2):
@@ -114,24 +122,41 @@ def drawPoly(Poly1):
     ax = fig.add_subplot(111)
     patch = patches.PathPatch(path, facecolor='orange', lw=2)
     ax.add_patch(patch)
-    ax.set_xlim(-1,len(verts)-1)
-    ax.set_ylim(-1,len(verts)-1)
+    ax.set_xlim(0,len(verts))
+    ax.set_ylim(0,len(verts))
     plt.show()
 
+def drawPoly2(list):
+    a=Vscpoly(len(list))
+    a.selectedpoints=[ Point(p[0],p[1]) for p in list]
+    drawPoly(a)
 
-polys=[Vscpoly(10)]
-for i in range(0,100):
+polys=[Vscpoly(223)]
+for i in range(0,3000):
     if len(polys)>0:
         poly1=polys.pop()
+        if i % 100 == 0:  print "Step " + str(i) + poly1.printPoly()
         #print "ITERATION "+str(i)+" with size of "+str(len(polys))
         #print poly1.printPoly()
         if len(poly1.selectedpoints)==poly1.size:
-           solutions.append(poly1)
-           print "solution found with area: "+str(findArea(poly1))+"\t"+poly1.printPoly()
-           drawPoly(poly1)
+            #closing the polygon may be a problem
+            lastslope=getslope(poly1.selectedpoints[-1],Point(0,0))
+            if lastslope not in poly1.slopesused and not doesintersect(poly1.selectedpoints[-1],Point(0,0),poly1.selectedpoints[1:]):
+                solutions.append(poly1)
         else:
             newpolys=mutateRandom(poly1)
             polys = polys+newpolys
 
+solutions.sort(key=lambda x: findArea(x), reverse=True)
 
+for s in solutions:
+    print "solution found with area: " + str(findArea(s)) + "\t" + s.printPolyAli()
 
+drawPoly(solutions[0])
+drawPoly(solutions[-1])
+print solutions[0].printPolyAli()+str(findArea(solutions[0]))
+print solutions[-1].printPolyAli()+str(findArea(solutions[-1]))
+#[(0, 0), (1, 4), (3, 1), (2, 3), (4, 2), (0.0, 0.0)]
+#solution found with area: 5.0	polysize:5	linelength:60.0	points:[(0,0), (2,4), (3,3), (1,2), (4,1)]	points:[(1,1), (3,5), (4,4), (2,3), (5,2)]
+
+print doesintersect(Point(1,2),Point(0,0),[ Point(2,1), Point(4,3), Point(1,2)])
