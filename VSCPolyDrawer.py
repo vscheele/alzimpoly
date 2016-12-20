1dd1cb79ccf02fa032bec46e0a0182ad68641ab4
vscheele <valentinscheele@gmail.com>
2016-12-20 13:49:21 +0100

    Added new Polydrawer

--- VSCPolyDrawer.py
+++ VSCPolyDrawer.py
@@ -31,20 +31,21 @@ class PolyDrawer:
         self.fig = plt.figure()
         return True
     def drawNewPoly(self,listOfPoints,xmax=-1,ymax=-1):  # draws a Poly in the Window
-        verts = listOfPoints
-        verts.append(verts[0])  # add first point as last (close the polygon)
-        codes = [1]
-        codes[0] = Path.MOVETO # move to the point where we start drawing
-        for v in range(0, len(verts) - 2): # draw the lines
-            codes.append(Path.LINETO)
-        codes.append(Path.CLOSEPOLY)
-        path = Path(verts, codes)
         self.fig.clear()
-        ax = self.fig.add_subplot(111)
-        patch = patches.PathPatch(path, facecolor='orange', lw=2)
-        ax.add_patch(patch)
-        ax.set_xlim(0, max([x[0] for x in verts]) )  # x
-        ax.set_ylim(0, max([x[1] for x in verts]) )  # y
+        if listOfPoints:
+            verts = listOfPoints
+            verts.append(verts[0])  # add first point as last (close the polygon)
+            codes = [1]
+            codes[0] = Path.MOVETO # move to the point where we start drawing
+            for v in range(0, len(verts) - 2): # draw the lines
+                codes.append(Path.LINETO)
+            codes.append(Path.CLOSEPOLY)
+            path = Path(verts, codes)
+            ax = self.fig.add_subplot(111)
+            patch = patches.PathPatch(path, facecolor='orange', lw=2)
+            ax.add_patch(patch)
+            ax.set_xlim(0, max([x[0] for x in verts]) )  # x
+            ax.set_ylim(0, max([x[1] for x in verts]) )  # y
         plt.grid()
         plt.pause(.1)
 
@@ -58,6 +59,7 @@ def main():
 
     pd=PolyDrawer()
     pd.createWindow()
+    pd.drawNewPoly([])
     pointlist=[(1,1),(2,2),(4,1),(4,0.5)]
     pd.drawNewPoly(pointlist)
     pointlist=[(1,0),(2,2),(4,1),(3,0.5)]
